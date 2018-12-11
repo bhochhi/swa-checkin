b0a35c7cb7508f94ea36335f9434af0afb251bcb
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-12-10 20:18:56 -0600

    bootstrap checkin page

--- swa-checkin/app.py
+++ swa-checkin/app.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+# @app.route("/")
+# def main():
+#     return render_template('index.html')
 
-@app.route("/static")
-def static():
-    return url_for('static', filename='style.css')
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == "__main__":
     app.run()
-
